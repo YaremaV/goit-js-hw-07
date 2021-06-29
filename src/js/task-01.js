4b12123bfb90c2f3b954bdb49906403384dff597
YaremaV <Yaremenko1996v@gmail.com>
2021-06-29 20:58:12 +0300

    completted first task

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,11 +1,21 @@
-const categories = document.querySelector('#categories')
-const itemEl = categories.querySelectorAll('.item')
 
-console.log(`В списке ${itemEl.length} категории.`)
+const itemEl = document.querySelectorAll('.item');
+console.log(`В списке ${itemEl.length} категории.`);
+console.log('=================')
 
-const item = document.querySelector('.item')
-console.log(item.firstElementChild.textContent)
-console.log(item.lastElementChild)
+const items = document.querySelector('.item');
+const title = items.firstElementChild;
+console.log(`Категория: ${title.textContent}.`);
 
+const listItem = items.lastElementChild;
+console.log(`Количество элементов: ${listItem.querySelectorAll('li').length}.`);
+console.log('=================')
 
+const productsEl = items.nextElementSibling;
+console.log(`Категория: ${productsEl.querySelector('h2').textContent}.`);
+console.log(`Количество элементов: ${productsEl.querySelectorAll('li').length}.`);
+console.log('=================')
 
+const techEl = productsEl.nextElementSibling;
+console.log(`Категория: ${techEl.querySelector('h2').textContent}.`);
+console.log(`Количество элементов: ${techEl.querySelectorAll('li').length}.`);
