ea4e44b05b5b67e601a3d998c1378adf827fbbe6
YaremaV <Yaremenko1996v@gmail.com>
2021-07-01 23:56:13 +0300

    completed task -07

---
+++ src/js/task-07.js
@@ -0,0 +1,9 @@
+const fzControl = document.querySelector('#font-size-control');
+const textRef = document.querySelector('#text');
+
+
+function onInput(event) {
+   textRef.style.fontSize = event.currentTarget.value + "px";
+ }
+
+fzControl.addEventListener('change', onInput)
